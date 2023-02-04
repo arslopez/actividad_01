a30f71cdc39e843b132bf7868d530cfdfc171748
arslopez <arielisaibonilla@gmail.com>
2023-02-03 23:46:42 -0600

    Definicion y construccion de la peticion de datos en el menu

--- convert_coordenadas.py
+++ convert_coordenadas.py
@@ -6,7 +6,23 @@ def menu():
     opcion=int(input())
     if opcion==1:
         #coversexa()
-        #pedir_datos_sexa()
+        pedir_datos_sexa()
     elif opcion==2:
-        #pedir_datos_deci()
+        pedir_datos_deci()
 
+#peticion de datos en caso de ser sexagesimal
+def pedir_datos_sexa():
+    latsexa=input('Ingrese latitud en sexagesimal (grados minutos segundos): ')
+    lonsexa=input('Ingrese latitud en sexagesimal (grados minutos segundos): ')
+    sexa=covern(latsexa,lonsexa)
+    coversad(sexa)
+    return 0
+
+
+#peticion de datos en caso de ser decimal
+def pedir_datos_deci():
+    datdeclat=input('Ingrese latitud en decimales: ')
+    datdeclon=input('Ingrese longitud en decimales: ')
+    deci=covern(datdeclat,datdeclon)
+    coverdas(deci)
+    return 0
