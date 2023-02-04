8ff96f8cdaf2b0b5e5ab4e0161feb0e24f237df4
arslopez <arielisaibonilla@gmail.com>
2023-02-03 23:35:24 -0600

    Menu del programa

---
+++ convert_coordenadas.py
@@ -0,0 +1,12 @@
+#menu y contenedores básicos
+def menu():
+    print("Seleccione una opción que tipo de conversión necesita:")
+    print("              1.DMS a DD")
+    print("              2.DD a DMS")
+    opcion=int(input())
+    if opcion==1:
+        #coversexa()
+        #pedir_datos_sexa()
+    elif opcion==2:
+        #pedir_datos_deci()
+
